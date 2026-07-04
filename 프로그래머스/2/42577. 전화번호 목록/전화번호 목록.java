566332e8991fc94299f97a8d194cfc51998176a4
codeok2 <87077866+codeok2@users.noreply.github.com>
2026-07-04 17:40:27 +0900

    [level 2] Title: 전화번호 목록, Time: 197.14 ms, Memory: 230 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,15 +1,22 @@
-import java.util.*;
+import java.util.Set;
+import java.util.HashSet;
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        Arrays.sort(phone_book);
+        Set<String> set = new HashSet<>();
+        for (String number : phone_book) {
+            set.add(number);
+        }
         
-        for (int i = 0; i < phone_book.length-1; i++) {
-            if (phone_book[i+1].startsWith(phone_book[i])) {
-                answer = false;
-                break;
+        // 본인 접두어가 집합에 있는지(해시 활용)
+        for (String number: phone_book) {
+            for (int i = 1; i < number.length(); i++) {
+                String part = number.substring(0, i);
+                if (set.contains(part)) {
+                    return false;
+                }
             }
         }
-        return answer;
+        
+        return true;
     }
 }
