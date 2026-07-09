18af56f44020ded2dd26dc1dceadd6b83c1380c2
codeok2 <87077866+codeok2@users.noreply.github.com>
2026-07-09 16:20:34 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.81 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,49 +1,23 @@
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
-        for (int i = 0; i < n; i++) {
-            answer[i] = "";
-        }        
-
-        int num1 = 1;
-        for (int i = 0; i < n-1; i++) {
-            num1 *= 2;
-        }
+        int[] tmp = new int[n];
         
-        int[][] map1 = new int[n][n];
         for (int i = 0; i < n; i++) {
-            int cmp = num1;
-            for (int j = 0; j < n; j++) {
-                if (arr1[i] >= cmp) {
-                    map1[i][j] = 1;
-                    arr1[i] -= cmp;
-                }
-                cmp /= 2;
-            }
-        }
-        
-        int[][] map2 = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            int cmp = num1;
-            for (int j = 0; j < n; j++) {
-                if (arr2[i] >= cmp) {
-                    map2[i][j] = 1;
-                    arr2[i] -= cmp;
-                }
-                cmp /= 2;
-            }
+            tmp[i] = arr1[i] | arr2[i];
         }
         
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (map1[i][j] == 1 || map2[i][j] == 1) {
-                    answer[i] = answer[i].concat("#");
-                } else {
-                    answer[i] = answer[i].concat(" ");
+            String s = Integer.toBinaryString(tmp[i]);    
+            int check = n - s.length();
+            if (check != 0) {
+                while (check-- > 0) {
+                    s = "0" + s;
                 }
             }
+            answer[i] = s.replace("1", "#").replace("0", " ");
         }
+    
         return answer;
     }
-    
 }
